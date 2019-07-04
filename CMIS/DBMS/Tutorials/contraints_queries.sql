c850162c7d5cf7e939fb5fc8b743e3100735f700
Chamod Shehanka <hcsperera@outlook.com>
2019-07-04 05:59:01 -0400

    Create contraints_queries.sql

---
+++ CMIS/DBMS/Tutorials/contraints_queries.sql
@@ -0,0 +1,25 @@
+USE U172117;
+
+CREATE TABLE Person(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(222) NOT NULL, 
+	last_name VARCHAR(222) NOT NULL,
+	address VARCHAR(222) NOT NULL,
+	city VARCHAR(222) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO Person VALUES 
+(2,'vikas','sharma','mohail','mohal'),
+(3, 'jaskaran','singhe','mullaput','mohail');
+
+CREATE TABLE IF NOT EXISTS `Orders`(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	order_no INT(11) NOT NULL,
+	person_id INT(11) NOT NULL,
+	PRIMARY KEY(id),
+	KEY orders_ibfk_1(person_id)
+);
+
+ALTER TABLE orders
+ADD CONSTRAINT orders_ibfk_1 FOREIGN
